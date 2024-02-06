4db4abe7783227e61ded1a47b4fd5dea44a7f7ec
Tyler Mayoff <tyler@tylermayoff.com>
2024-02-05 21:31:29 -0500

    Added clang-tidy

--- wren/include/wren/utils/enums.hpp
+++ wren/include/wren/utils/enums.hpp
@@ -3,7 +3,7 @@
 #include <string>
 #include <boost/describe.hpp>
 
-template <class E> std::string enum_to_string(E e) {
+template <class E> auto enum_to_string(E e) -> std::string {
   std::string r = "(unnamed)";
   boost::mp11::mp_for_each<boost::describe::describe_enumerators<E>>(
       [&](auto D) {
@@ -13,6 +13,7 @@ template <class E> std::string enum_to_string(E e) {
   return r;
 }
 
+// NOLINTNEXTLINE
 #define DESCRIBED_ENUM(E, ...)                                                 \
   enum class E { __VA_ARGS__ };                                                \
   BOOST_DESCRIBE_ENUM(E, __VA_ARGS__)
