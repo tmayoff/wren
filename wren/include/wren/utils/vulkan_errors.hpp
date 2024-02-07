c43902c148fd7fe996336f8765def4404926500e
Tyler Mayoff <tyler@tylermayoff.com>
2024-02-07 08:30:12 -0500

    Fixed multiple definition error

--- wren/include/wren/utils/vulkan_errors.hpp
+++ wren/include/wren/utils/vulkan_errors.hpp
@@ -6,9 +6,7 @@
 #include <vulkan/vulkan_enums.hpp>
 #include <vulkan/vulkan_to_string.hpp>
 
-template <> auto enum_to_string<vk::Result>(vk::Result e) -> std::string {
-  return vk::to_string(e);
-}
+template <> auto enum_to_string<vk::Result>(vk::Result e) -> std::string;
 
 ERROR_CODE(vk, Result, eInitializationFailed, eSuccess, eOutofHostMemory)
 ERROR_ENUM(wren, VulkanErrors, NoDevicesFound, QueueFamilyNotSupported)
