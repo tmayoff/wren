2f1a92b3adf993eba50319136998cdf6eec2ac66
Tyler Mayoff <tyler@tylermayoff.com>
2024-09-20 21:31:01 -0400

    Setup second render graph

--- editor/src/main.cpp
+++ editor/src/main.cpp
@@ -69,84 +69,79 @@ auto main() -> int {
 
 void Scene::on_update() {
   ZoneScoped;  // NOLINT
-  // editor::ui::begin();
+  editor::ui::begin();
 
-  // static ImGuiDockNodeFlags dockspace_flags =
-  // ImGuiDockNodeFlags_None; ImGuiWindowFlags window_flags =
-  // ImGuiWindowFlags_None;
+  static ImGuiDockNodeFlags dockspace_flags = ImGuiDockNodeFlags_None;
+  ImGuiWindowFlags window_flags = ImGuiWindowFlags_None;
 
-  // ImGuiViewport const *viewport = ImGui::GetMainViewport();
-  // ImGui::SetNextWindowPos(viewport->WorkPos);
-  // ImGui::SetNextWindowSize(viewport->WorkSize);
-  // ImGui::SetNextWindowViewport(viewport->ID);
-  // ImGui::PushStyleVar(ImGuiStyleVar_WindowRounding, 0.0F);
-  // ImGui::PushStyleVar(ImGuiStyleVar_WindowBorderSize, 0.0F);
+  ImGuiViewport const *viewport = ImGui::GetMainViewport();
+  ImGui::SetNextWindowPos(viewport->WorkPos);
+  ImGui::SetNextWindowSize(viewport->WorkSize);
+  ImGui::SetNextWindowViewport(viewport->ID);
+  ImGui::PushStyleVar(ImGuiStyleVar_WindowRounding, 0.0F);
+  ImGui::PushStyleVar(ImGuiStyleVar_WindowBorderSize, 0.0F);
 
-  // window_flags |=
-  //     ImGuiWindowFlags_NoTitleBar | ImGuiWindowFlags_NoCollapse |
-  //     ImGuiWindowFlags_NoResize | ImGuiWindowFlags_NoMove |
-  //     ImGuiWindowFlags_NoBringToFrontOnFocus |
-  //     ImGuiWindowFlags_NoNavFocus;
+  window_flags |=
+      ImGuiWindowFlags_NoTitleBar | ImGuiWindowFlags_NoCollapse |
+      ImGuiWindowFlags_NoResize | ImGuiWindowFlags_NoMove |
+      ImGuiWindowFlags_NoBringToFrontOnFocus |
+      ImGuiWindowFlags_NoNavFocus;
 
-  // ImGui::Begin("Editor", nullptr, window_flags);
-  // ImGui::PopStyleVar(2);
+  ImGui::Begin("Editor", nullptr, window_flags);
+  ImGui::PopStyleVar(2);
 
-  // ImGuiIO &io = ImGui::GetIO();
-  // if (io.ConfigFlags & ImGuiConfigFlags_DockingEnable) {
-  //   ImGuiID dockspace_id = ImGui::GetID("Editor");
-  //   ImGui::DockSpace(dockspace_id, ImVec2{0.0F, 0.0F},
-  //                    dockspace_flags);
+  ImGuiIO &io = ImGui::GetIO();
+  if (io.ConfigFlags & ImGuiConfigFlags_DockingEnable) {
+    ImGuiID dockspace_id = ImGui::GetID("Editor");
+    ImGui::DockSpace(dockspace_id, ImVec2{0.0F, 0.0F},
+                     dockspace_flags);
 
-  //   static bool first_time = true;
-  //   if (first_time) {
-  //     first_time = false;
+    static bool first_time = true;
+    if (first_time) {
+      first_time = false;
 
-  //     ImGui::DockBuilderRemoveNode(dockspace_id);
-  //     ImGui::DockBuilderAddNode(
-  //         dockspace_id,
-  //         dockspace_flags | ImGuiDockNodeFlags_DockSpace);
-  //     ImGui::DockBuilderSetNodeSize(dockspace_id, viewport->Size);
+      ImGui::DockBuilderRemoveNode(dockspace_id);
+      ImGui::DockBuilderAddNode(
+          dockspace_id,
+          dockspace_flags | ImGuiDockNodeFlags_DockSpace);
+      ImGui::DockBuilderSetNodeSize(dockspace_id, viewport->Size);
 
-  //     auto const dockspace_id_bottom = ImGui::DockBuilderSplitNode(
-  //         dockspace_id, ImGuiDir_Down, 0.25, nullptr,
-  //         &dockspace_id);
+      auto const dockspace_id_bottom = ImGui::DockBuilderSplitNode(
+          dockspace_id, ImGuiDir_Down, 0.25, nullptr, &dockspace_id);
 
-  //     auto const dockspace_id_right =
-  //         ImGui::DockBuilderSplitNode(dockspace_id, ImGuiDir_Right,
-  //                                     0.25F, nullptr,
-  //                                     &dockspace_id);
-  //     auto const dockspace_id_left = ImGui::DockBuilderSplitNode(
-  //         dockspace_id, ImGuiDir_Left, 0.25F, nullptr,
-  //         &dockspace_id);
+      auto const dockspace_id_right =
+          ImGui::DockBuilderSplitNode(dockspace_id, ImGuiDir_Right,
+                                      0.25F, nullptr, &dockspace_id);
+      auto const dockspace_id_left = ImGui::DockBuilderSplitNode(
+          dockspace_id, ImGuiDir_Left, 0.25F, nullptr, &dockspace_id);
 
-  //     ImGui::DockBuilderDockWindow("Inspector",
-  //     dockspace_id_right); ImGui::DockBuilderDockWindow("Viewer",
-  //     dockspace_id); ImGui::DockBuilderDockWindow("Scene",
-  //     dockspace_id_left);
-  //     ImGui::DockBuilderDockWindow("Filesystem",
-  //     dockspace_id_bottom);
+      ImGui::DockBuilderDockWindow("Inspector", dockspace_id_right);
+      ImGui::DockBuilderDockWindow("Viewer", dockspace_id);
+      ImGui::DockBuilderDockWindow("Scene", dockspace_id_left);
+      ImGui::DockBuilderDockWindow("Filesystem", dockspace_id_bottom);
 
-  //     ImGui::DockBuilderFinish(dockspace_id_bottom);
-  //   }
-  // }
+      ImGui::DockBuilderFinish(dockspace_id_bottom);
+    }
+  }
 
-  // ImGui::PushStyleVar(ImGuiStyleVar_WindowMinSize, ImVec2{500,
-  // 100}); ImGui::Begin("Scene"); ImGui::End();
+  ImGui::PushStyleVar(ImGuiStyleVar_WindowMinSize, ImVec2{500, 100});
+  ImGui::Begin("Scene");
+  ImGui::End();
 
-  // ImGui::Begin("Viewer");
-  // ImGui::Text("%s", "Scene go here");
-  // ImGui::End();
+  ImGui::Begin("Viewer");
+  ImGui::Text("%s", "Scene go here");
+  ImGui::End();
 
-  // ImGui::Begin("Inspector");
-  // ImGui::End();
+  ImGui::Begin("Inspector");
+  ImGui::End();
 
-  // ImGui::Begin("Filesystem");
-  // ImGui::End();
+  ImGui::Begin("Filesystem");
+  ImGui::End();
 
-  // ImGui::End();
-  // ImGui::PopStyleVar();
+  ImGui::End();
+  ImGui::PopStyleVar();
 
-  // editor::ui::end();
+  editor::ui::end();
 }
 
 auto Scene::build_ui_render_graph(
@@ -160,7 +155,11 @@ auto Scene::build_ui_render_graph(
                  wren::shaders::MESH_VERT_SHADER.data(),
                  wren::shaders::MESH_FRAG_SHADER.data()));
 
-  auto target = std::make_shared<wren::RenderTarget>();
+  VK_NS::ImageView image_view;
+
+  auto target = std::make_shared<wren::RenderTarget>(
+      VK_NS::Extent2D{245, 256}, VK_NS::Format::eB8G8R8A8Srgb,
+      VK_NS::SampleCountFlagBits::e1, image_view);
   mesh.shader(mesh_shader);
 
   builder
