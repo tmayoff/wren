9b573d16abbd304eda5a278be244f54ee9d3021c
Tyler Mayoff <tyler@tylermayoff.com>
2024-11-02 11:05:36 -0400

    Added test

--- wren_utils/tests/binary_reader.cpp
+++ wren_utils/tests/binary_reader.cpp
@@ -1,7 +1,19 @@
 #include <boost/test/unit_test.hpp>
+#include <wren/utils/binray_reader.hpp>
 
-BOOST_AUTO_TEST_SUITE(BinaryReader)
+BOOST_AUTO_TEST_SUITE(binary_reader)
 
-BOOST_AUTO_TEST_CASE(Read) {}
+BOOST_AUTO_TEST_CASE(Read) {
+  std::vector<uint8_t> input = {
+      0x53, 0x53, 0x53, 0x53, 0x53, 0x53, 0x53, 0x53, 0x53, 0x53,
+  };
+
+  std::span span(input);
+
+  auto reader = wren::utils::BinaryReader(span);
+  reader.skip(10);
+
+  BOOST_TEST(reader.at_end());
+}
 
 BOOST_AUTO_TEST_SUITE_END();
