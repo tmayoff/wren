1ac835bf900077267f8fd4e0497f1aeeb0bb774e
Tyler Mayoff <tyler@tylermayoff.com>
2024-11-02 09:59:28 -0400

    More coverage

--- wren_utils/tests/string_reader.cpp
+++ wren_utils/tests/string_reader.cpp
@@ -6,7 +6,7 @@ BOOST_AUTO_TEST_SUITE(string_reader)
 BOOST_AUTO_TEST_CASE(Read) {
   std::string_view input = R"(
 hello world this is tyler
-    
+key: value
 )";
 
   wren::utils::StringReader reader(input);
@@ -14,6 +14,12 @@ hello world this is tyler
   reader.skip_to_text_end("this is ");
   const auto name = reader.read_to_end_line();
   BOOST_TEST(name == "tyler");
+
+  const auto key = reader.read_to_text_start(": ");
+  BOOST_TEST(key == "key");
+  reader.skip_to_text_end(": ");
+  const auto val = reader.read_to_end_line();
+  BOOST_TEST(val == "value");
 }
 
 BOOST_AUTO_TEST_SUITE_END();
