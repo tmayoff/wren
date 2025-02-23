b31ecde5e28de7dc50a365c6681ff93b5563dac6
Tyler Mayoff <tyler@tylermayoff.com>
2025-02-23 09:53:01 -0500

    unformatted

--- editor/src/main.cpp
+++ editor/src/main.cpp
@@ -33,7 +33,11 @@ auto main(int argc, char** argv) -> int {
   const auto& err = wren::Application::Create("Editor");
   if (!err.has_value()) {
     spdlog::error("failed to create application: {}", err.error().message());
-    return EXIT_FAILURE;
+
+
+
+
+            return EXIT_FAILURE;
   }
 
   auto app = err.value();
